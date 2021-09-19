583aafe75f7f5c39e4066a564434d0046555beb8
Suyash Shakya <suyash.shakya@dragonlaw.com.hk>
2021-09-19 13:31:35 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {makeStyles} from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Background from './images/pattern-background-desktop.svg';
+import DesktopBackground from './images/pattern-background-desktop.svg';
+import MobileBackground from './images/pattern-background-mobile.svg';
 import Hero from './images/illustration-hero.svg';
 import musicIcon from './images/icon-music.svg';
 
@@ -16,9 +17,13 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     height: '100vh',
     width: 'auto',
-    backgroundImage: `url(${Background})`,
+    backgroundImage: `url(${DesktopBackground})`,
+    backgroundColor: 'hsl(225, 100%, 94%)',
     backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain'
+    backgroundSize: 'contain',
+    [theme.breakpoints.down('sm')]: {
+      backgroundImage: `url(${MobileBackground})`,
+    },
   },
   cardRoot: {
     margin: 50,
@@ -36,13 +41,35 @@ const useStyles = makeStyles((theme) => ({
     padding: 20,
     margin: 20, 
     width: 'auto',
-    backgroundColor: '#e1edec',
+    backgroundColor: '#f0faf9',
     borderRadius: 2,
     [theme.breakpoints.down('sm')]: {
       marginLeft: 5,
       marginRight: 5
     },
-  }, 
+  },
+  changeText: {
+    textDecoration: 'underline',
+    color: 'hsl(245, 75%, 52%)',
+    "&:hover": {
+      textDecoration: 'none',
+      color: 'hsl(224, 23%, 55%)'
+    }
+  },
+  cancelOrder: {
+    color: 'hsl(224, 23%, 55%)',
+    "&:hover": {
+      color: '#000000',
+    }
+  },
+  commonButton: {
+    textTransform: 'none'
+  },
+  paymentButton: {
+    textTransform: 'none', 
+    padding: 10, 
+    borderRadius: 10
+  },
   media: {
     height: 200,
   },
@@ -76,23 +103,23 @@ const App = () => {
                 <Typography variant='subtitle1' color='textSecondary'>$59.99/year</Typography>
               </Box>
               </Box>
-              <Button>
-                <Typography variant='subtitle2' color='primary'>
-                  <b><u>Change</u></b>
+              <Button className={classes.commonButton}>
+                <Typography variant='subtitle2' className={classes.changeText}>
+                  <b>Change</b>
                 </Typography>
               </Button>
             </Box>
           </Box>
           <Box px={4}>
-            <Button color='primary' variant='contained' fullWidth style={{textTransform: 'none', padding: 10, borderRadius: 10}}>
+            <Button color='primary' variant='contained' fullWidth className={classes.paymentButton}>
               <Typography variant='subtitle2'>
                 Proceed to Payment
               </Typography>  
             </Button>
           </Box>
           <Box px={4} mt={2} display='flex' justifyContent='center'>
-            <Button style={{textTransform: 'none'}}>
-                <Typography variant='subtitle2' color='textSecondary'>
+            <Button variant='none' className={classes.commonButton}>
+                <Typography variant='subtitle2' className={classes.cancelOrder}>
                   <b>Cancel Order</b>
                 </Typography>  
             </Button>
